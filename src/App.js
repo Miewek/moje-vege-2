17c10a86c920f8e2a29db8e525048eafa1f0318e
annaanna71261 <38671291+annaanna71261@users.noreply.github.com>
2020-06-11 18:32:48 +0200

    wersja początkowa

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import StronaGlowna from "./StronaGlowna";
+import Przepis from "./Przepis";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <Router>
+            <div className="aplikacja">
+                <h1>Moje vege</h1>
+                <h2>Czyli moje wegańskie i wegetarianśkie przepisy</h2>
+
+                <Switch>
+                    <Route path="/:adres">
+                        <Przepis />
+                    </Route>
+                    <Route path="/">
+                        <StronaGlowna />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
