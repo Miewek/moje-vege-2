80c0f338443adab0a6ff8df39d305df709a4f83b
annaanna71261 <38671291+annaanna71261@users.noreply.github.com>
2020-06-16 05:52:11 +0200

    przepis

--- src/Przepis.js
+++ src/Przepis.js
@@ -2,3 +2,44 @@ import React from "react";
 
 import przepisy from "./przepisy.json";
 
+import { Link, useParams } from "react-router-dom";
+
+function Przepis ()
+{
+    let { adres } = useParams();
+    const dane = przepisy.find( ( przepis ) => przepis.adres === adres );
+    const obrazek = require( `./${ dane.obrazek }` );
+
+    return (
+        <div className="przepis">
+            <h1>{dane.nazwa}</h1>
+            <img className="obrazek" src={obrazek} alt={dane.nazwa} />
+
+            <strong>Składniki</strong>
+            <ul className="skladniki">
+                {dane.skladniki.map( ( skladnik ) => (
+                    <li>{skladnik}</li>
+                ) )}
+            </ul>
+
+            <span className="czas">Czas przygotowania: {dane.czas}</span>
+            <span className="porcje">Porcje: {dane.porcje}</span>
+
+            <strong>Przygotowanie</strong>
+            <ol className="przygotowanie">
+                {dane.przygotowanie.map( ( krok ) => (
+                    <li>{krok}</li>
+                ) )}
+            </ol>
+
+            <Link className="powrot" to={"/"}>
+                Powrót na stronę główną
+            </Link>
+        </div>
+    );
+}
+
+export default Przepis;
+
+
+
