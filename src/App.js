5492f92b8821a67a62abfd669ba148ee781ff75d
annaanna71261 <38671291+annaanna71261@users.noreply.github.com>
2020-06-18 09:29:56 +0200

    menu strony

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import Omnie from "./Omnie";
 import Kontakt from "./Kontakt";
 import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
     return (
         <Router>
             <div className="aplikacja">
+                <header className="naglowek">
+                    <Link className="menulink" to={"/"}>
+                        Strona główna
+                    </Link>
+                    <Link className="menulink" to={"/omnie"}>
+                        O mnie
+                    </Link>
+                    <Link className="menulink" to={"/kontakt"}>
+                       Kontakt
+                    </Link>
+                </header>
+                
                 <h1>Moje vege</h1>
                 <h2>Czyli moje wegańskie i wegetarianśkie przepisy</h2>
 
