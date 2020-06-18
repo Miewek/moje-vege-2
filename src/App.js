f23f71811cbbcf41505e06598b6ff60fc53ad01b
annaanna71261 <38671291+annaanna71261@users.noreply.github.com>
2020-06-18 08:47:41 +0200

    kontakt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import StronaGlowna from "./StronaGlowna";
 import Przepis from "./Przepis";
 import Omnie from "./Omnie";
+import Kontakt from "./Kontakt";
 import "./App.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -14,6 +15,9 @@ function App() {
                 <h2>Czyli moje wegańskie i wegetarianśkie przepisy</h2>
 
                 <Switch>
+                    <Route path="/kontakt">
+                        <Kontakt />
+                    </Route>
                     <Route path="/omnie">
                         <Omnie />
                     </Route>
---
+++ src/Kontakt.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Kontakt ()
+{
+    return (
+        <div>
+            Kontakt
+        </div>
+    );
+}
+
+export default Kontakt;
